e2b991346a829ee17fe3ce305841427dc02aed4d
NizuSAO <nizusao@gmail.com>
2022-06-03 16:15:39 -0300

    Archivitos

---
+++ yui1.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr 
+import pyttsx3
+
+listener = sr.Recognizer()
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+engine.say('Welcome-Nizu')
+engine.runAndWait()
+
+try:
+    with sr.Microphone() as source:
+        print("Escuchando...")
+        voice = listener.listen(source)
+        rec = listener.recognize_google(voice)
+        print(rec)
+
+except:
+    pass 
