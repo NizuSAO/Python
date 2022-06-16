49cbaf5e18f26623aa4af637e175cbc4d1ee4626
NizuSAO <93949428+NizuSAO@users.noreply.github.com>
2022-06-16 19:54:47 -0300

    Create operadores.py

---
+++ Introduccionpython/operadores/operadores.py
@@ -0,0 +1,22 @@
+#operadores nos ayudan a realizar operaciones como su nombre indica
+#usamos la palabra print para que se imprima en consola lo que vayamos a realizar
+
+#suma
+print(5+8)
+#resta
+print(6-3)
+#multiplicacion
+print(3*3)
+#division
+print(6/2)
+#porcentaje
+print(100%10)
+#potencia
+print(3**3)
+
+#incremento
+print(3+=)
+#decremento
+print(3-=)
+
+#estos son algunos ejemplos de como usar operadores en python
