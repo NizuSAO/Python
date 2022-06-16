e38eae4260d93c3ebf6d4937cc36cceb83d87e6d
NizuSAO <93949428+NizuSAO@users.noreply.github.com>
2022-06-16 20:06:48 -0300

    Create variable.py

---
+++ Introduccionpython/variables/variable.py
@@ -0,0 +1,22 @@
+#como bien dijimos las variables son espacios de memoria del ordenador donde podemos almacenar valores
+
+caja1 = 5
+caja2 = 7
+
+#dentro de caja1 tenemos el numero 5 y en caja2 el numero 7
+#entonces cada vez que necesitemos usar el 5 o el 7 podemos poner caja1 o caja2
+
+caja3 = caja1 + caja2
+
+#ahora en la caja3 decimos que lo que hay adentro es la suma de la caja1 con la caja2
+
+#Entonces la variable es una forma de almacenar un valor en un espacio de memoria.
+
+#cuando querramos saber cuanto es la caja1 + caja2.
+
+print(caja3)
+
+#en lugar de hacer
+
+print(5 + 7)
+print(caja1 + caja2)
