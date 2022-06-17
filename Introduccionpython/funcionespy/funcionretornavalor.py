5faac1c5ed9352a21b8f2cb1b16b9acd0508618c
NizuSAO <93949428+NizuSAO@users.noreply.github.com>
2022-06-17 02:26:21 -0300

    Create funcionretornavalor.py

---
+++ Introduccionpython/funcionespy/funcionretornavalor.py
@@ -0,0 +1,10 @@
+def maquina_dispensadora(num1, num2)
+
+       resultado= num1 + num2
+
+        return resultado
+
+Papas=maquina_dispensadora(5,8)
+Pepsi=mauqina_dispensadora(7,8)
+chocolate=maquina_dispensadora(7,9)
+
